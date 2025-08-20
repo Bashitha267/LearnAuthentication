ae35dcfd8966d8f57af702e0afa4c111c06d8dfb
Nimesh Bashitha <2022csc023@univ.jfn.ac.lk>
2025-08-20 12:41:30 +0530

    Update mongodb.js

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
 
+let isConnected = false; // cache connection for serverless
+
 const connectDB = async () => {
+  if (isConnected) return;
+
   try {
-    await mongoose.connect("mongodb+srv://nimeshspc2k17:1234@cluster0.oxybnxt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      "mongodb+srv://nimeshspc2k17:1234@cluster0.oxybnxt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    isConnected = true;
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection failed:", err);
