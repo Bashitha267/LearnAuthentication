bd17ebb0415e654e4abe5eb557db6ced44a069f6
Nimesh <nimeshspc2k17.gmail.com>
2025-08-20 12:08:15 +0530

    modified:   config/mongodb.js

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect("mongodb+srv://nimeshspc2k17:1234@cluster0.oxybnxt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
